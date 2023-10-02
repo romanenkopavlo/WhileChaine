b428cfcc1da3639edc69bfca16903e27c110444d
utopiaforever <pavlo.romanenko@lyceeastier.com>
2023-10-02 22:56:29 +0200

    La fin de revision

--- src/ChaineWhile.java
+++ src/ChaineWhile.java
@@ -2,27 +2,41 @@ import clavier.In;
 
 public class ChaineWhile {
     public static void main(String[] args) {
-        String chaineSaisie = null;
-        char caractereSaisie;
-        int numeroDeCaracteres = 0; int laTaille = 0;
+        String chaineSaisie;
+        char caractereSaisie1, caractereSaisie2, caractereDeChaineSaisie;
+        int numeroDeCaracteres1 = 0, numeroDeCaracteres2 = 0, laTaille;
 
-        do {
-            System.out.print("Saisissez la phrase: ");
-            chaineSaisie = new String(In.readString());
+        try {
+            do {
+                System.out.print("Saisissez la phrase: ");
+                chaineSaisie = new String(In.readString());
 
-            System.out.print("Saisissez le caractere qui vous voulez compter dans votre phrase: ");
-            caractereSaisie = In.readChar();
+                System.out.print("Saisissez le premiere caractere qui vous voulez compter dans votre phrase: ");
+                caractereSaisie1 = In.readChar();
 
-            laTaille = chaineSaisie.length();
+                System.out.print("Saisissez le deuxieme caractere qui vous voulez compter dans votre phrase: ");
+                caractereSaisie2 = In.readChar();
 
-//            while (laTaille != 0) {
-//
-//            }
+                laTaille = chaineSaisie.length();
 
-        } while (chaineSaisie != null);
+                while (laTaille > 0) {
+                    caractereDeChaineSaisie = chaineSaisie.substring(laTaille - 1, laTaille).charAt(0);
+                    --laTaille;
+                    if (caractereDeChaineSaisie == caractereSaisie1) {
+                        numeroDeCaracteres1++;
+                    }
+                    if (caractereDeChaineSaisie == caractereSaisie2) {
+                        numeroDeCaracteres2++;
+                    }
+                }
+                System.out.println("Le numero de " + caractereSaisie1 + ": " + numeroDeCaracteres1);
+                System.out.println("Le numero de " + caractereSaisie2 + ": " + numeroDeCaracteres2);
 
-
-
-
+                numeroDeCaracteres1 = 0;
+                numeroDeCaracteres2 = 0;
+            } while (chaineSaisie != null);
+        } catch (NullPointerException exception) {
+            System.out.print("Vous n'avez rien saisi");
+        }
     }
 }
